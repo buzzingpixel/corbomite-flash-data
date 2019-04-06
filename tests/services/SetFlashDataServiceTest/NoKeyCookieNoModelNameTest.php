aa33787b4b2b9a5b34c75564b667f7ead5e735e9
TJ Draper <tj@buzzingpixel.com>
2019-04-06 11:00:14 -0500

    Update NoKeyCookieNoModelNameTest to be compatible with merge

--- tests/services/SetFlashDataServiceTest/NoKeyCookieNoModelNameTest.php
+++ tests/services/SetFlashDataServiceTest/NoKeyCookieNoModelNameTest.php
@@ -49,17 +49,17 @@ class NoKeyCookieNoModelNameTest extends TestCase
 
         $ormFactory = self::createMock(Factory::class);
 
-        $uuid4 = self::createMock(UuidInterface::class);
+        $uuid1 = self::createMock(UuidInterface::class);
 
-        $uuid4->expects(self::once())
+        $uuid1->expects(self::once())
             ->method('toString')
             ->willReturn('uuidString');
 
         $uuidFactory = self::createMock(UuidFactoryInterface::class);
 
         $uuidFactory->expects(self::once())
-            ->method('uuid4')
-            ->willReturn($uuid4);
+            ->method('uuid1')
+            ->willReturn($uuid1);
 
         $flashDataStoreModel = self::createMock(FlashDataStoreModelInterface::class);
 
